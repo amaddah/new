bbf7f9df01457b50e2bbc530fcc0df2c5d6fd786
amaddah <amine.maddah@insa-cvl.fr>
2015-03-29 14:55:00 +0200

    makefile

--- lib/c/default.c
+++ lib/c/default.c
@@ -1,6 +1,26 @@
-#include <stdio.h>
+#include "define.h"
 
-int main (int argc, const char * argv[]){
-	
-	return 0;
+int main(int argc, char *argv[], char **arge)
+{
+	atexit(fin_programme);
+
+	/* Variables */
+
+
+
+	/* Debut Script */
+
+
+	/* Fin Script */
+
+	return EXIT_SUCCESS;
 }
+
+void fin_programme()
+	{
+
+		printf("A la prochaine !\n");
+		__ATTENTE__
+	}
+
+/* ISO-90 : ligne de fin */
